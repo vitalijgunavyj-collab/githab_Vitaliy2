4213ef3be4d7e30ba9c4fdc4c6993b7ed3d17fe6
“John <john@email.com>
2026-03-17 12:31:43 +0200

    created first python file

---
+++ main.py
@@ -0,0 +1,12 @@
+import poems
+import sign
+author_name = "Віталій"
+print(sign.SIGN_TEMPLATE.format(author_name))
+print()
+
+print(poems.poem1)
+print()
+print(poems.poem2)
+
+wishes = "Більше практики\nЦікаві пояснення\nМенше складних тем"
+print(wishes)
