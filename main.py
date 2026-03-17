a8f7f90bb1137ea681e1e827941f374887dd6757
“John <john@email.com>
2026-03-17 12:41:11 +0200

    created first python file

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import sign
 author_name = "Віталій"
 print(sign.SIGN_TEMPLATE.format(author_name))
 print()
-
-print(poems.poem1)
-print()
-print(poems.poem2)
-
 wishes = "Більше практики\nЦікаві пояснення\nМенше складних тем"
 print(wishes)
+stars= "************"
+print(stars)
+print(poems.poem1)
+print(stars)
+print(poems.poem2)
+print(stars)
