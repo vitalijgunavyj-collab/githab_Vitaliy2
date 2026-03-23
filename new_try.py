a51e044f348d5409be02154ebd6371fa0c821047
“John <john@email.com>
2026-03-23 21:14:23 +0200

    created first python file

---
+++ new_try.py
@@ -0,0 +1,47 @@
+apples = "3"
+pears = "2"
+
+# print(type(apples))
+
+
+apples = int(apples)
+
+integer_number = 1_0009
+
+integer_number2 = 10_001
+
+suma = integer_number + integer_number2
+print(suma)
+print("Сума", suma,'in total')
+print(f"{suma=}")
+print(f"{2+2=}")
+
+pears = int(pears)
+
+total_fruits = apples + pears
+
+# total_fruits_cost = total_fruits * "156"
+
+print(total_fruits)
+n1 = 12
+n2 = 244
+difference = n1 - n2
+print(f"{difference=}")
+
+n3 = -300
+n4 = -200
+mult = n1 * n3
+print(f"{mult=}")
+
+n5 = 20
+n6 = 4
+faction = n5 / n6
+print(f"{faction=}")
+
+not_interger = 0.5
+not_interger_2 = 0.5
+not_interger_suma = not_interger +  not_interger_2 + integer_number2
+print(f"{not_interger_suma=}")
+
+
+
